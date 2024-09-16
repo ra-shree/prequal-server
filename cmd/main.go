072790dafc004496aee77d3cdb5d789bd5d45564
Aarya Shree Chapagain <96831449+ra-shree@users.noreply.github.com>
2024-09-16 21:30:26 +0545

    Implementing reverse proxy (#4)
    * Added Probe Structure
    
    * Clean Project
    removed:all code besides the data structure for probe
    
    * Updating ReverseProxy package
    added:multi-host proxy
    added:round-robin for selecting replicas
    added:http and https support
    
    * Changed project structure
    
    * Added logic for implementing probing with server replica

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,34 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/signal"
 
-	"github.com/ra-shree/prequal-server/internal/server"
+	"github.com/gorilla/mux"
+	"github.com/ra-shree/prequal-server/pkg/reverseproxy"
 )
 
 func main() {
-	if err := server.Run(); err != nil {
-		log.Printf("Could not start the server: %v", err)
+	proxy := &reverseproxy.ReverseProxy{}
+
+	r := mux.NewRouter()
+	r.Host("localhost").PathPrefix("/api")
+
+	proxy.AddReplica([]string{"http://localhost:9000"}, r)
+
+	proxy.AddReplica([]string{
+		"http://localhost:9001",
+		"http://localhost:9002",
+		"http://localhost:9003",
+	}, nil)
+
+	proxy.AddReplica([]string{"http://localhost:8000"}, r)
+	proxy.AddListener(":8080")
+	if err := proxy.Start(); err != nil {
+		log.Fatal(err)
 	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
 }
