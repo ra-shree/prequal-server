d6731c5272a859dd4b26d3d39486e8f15e00ee85
Aarya Shree Chapagain <caaryashree@gmail.com>
2024-12-09 21:13:52 +0545

    Added add replica and remove replica message publishing to admin server

--- messaging/utils.go
+++ messaging/utils.go
@@ -3,8 +3,8 @@ package messaging
 import (
 	"encoding/json"
 	"log"
+	"net/url"
 
-	"github.com/gorilla/mux"
 	"github.com/ra-shree/prequal-server/common"
 	"github.com/ra-shree/prequal-server/reverseproxy"
 )
@@ -28,64 +28,46 @@ func processMessage(body []byte) {
 		return
 	}
 
+	type ReplicaMessage struct {
+		Name                string `json:"name"`
+		Url                 string `json:"url"`
+		Status              string `json:"status"`
+		HealthCheckEndpoint string `json:"healthcheckendpoint"`
+	}
+
+	bodyBytes, err := json.Marshal(msg.Body)
+	if err != nil {
+		log.Printf("Failed to marshal message body: %v", err)
+		return
+	}
+
+	var replica ReplicaMessage
+	err = json.Unmarshal(bodyBytes, &replica)
+
 	switch {
 	case msg.Name == ADD_REPLICA:
-		type ReplicaMessage struct {
-			Name                string `json:"name"`
-			Url                 string `json:"url"`
-			Status              string `json:"status"`
-			HealthCheckEndpoint string `json:"healthcheckendpoint"`
-		}
-		bodyBytes, err := json.Marshal(msg.Body)
-		if err != nil {
-			log.Printf("Failed to marshal message body: %v", err)
-			return
-		}
-
-		var replica ReplicaMessage
 		// err := json.Unmarshal([]byte(msg.Body.(string)), &replica)
-		err = json.Unmarshal(bodyBytes, &replica)
 		if err != nil {
-			message := Message{
-				Name: REPLICA_ADD_FAILED,
-				Body: msg.Body,
-			}
-
-			PublishMessage(PUBLISHING_QUEUE, &message)
+			ReplicaAddFailed(replica.Url)
 			return
 		}
 
 		reverseproxy.Proxy.Replicas[0].AddUpstream(replica.Url)
-		// reverseproxy.Proxy.
-		message := Message{
-			Name: ADDED_REPLICA,
-			Body: msg.Body,
-		}
 		common.AppendNewReplica(replica.Url)
-		// AddUpstream(replica.Url, common.MuxRouter)
-		PublishMessage(PUBLISHING_QUEUE, &message)
-		return
-		// addReplica(&replica)
 
+		// send the message to the admin server
+		ReplicaAdded(replica.Url)
+	case msg.Name == REMOVE_REPLICA:
+		url, err := url.Parse(replica.Url)
+
+		if err != nil {
+			log.Printf("error parsing url for removal in message consumer: %v", err)
+		}
+
+		reverseproxy.Proxy.Replicas[0].RemoveUpstream(url)
+		ReplicaRemoved(replica.Url)
 	}
-	log.Print("Received a message \n")
-	log.Printf("Name: %v", msg.Name)
-	log.Printf("Body: %v", msg.Body)
-}
-
-func AddUpstream(upstream string, router *mux.Router) {
-	// url, err := url.Parse(upstream)
-
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// router := mux.NewRouter()
-
-	router.Host("localhost").PathPrefix("/")
-
-	reverseproxy.Proxy.Replicas[0].AddUpstream(upstream)
-	log.Printf("Added upstream: %s\n", upstream)
-	// t.Upstreams = append(t.Upstreams, url)
-
-	// return t.Upstreams, nil
+	// log.Print("Received a message \n")
+	// log.Printf("Name: %v", msg.Name)
+	// log.Printf("Body: %v", msg.Body)
 }
